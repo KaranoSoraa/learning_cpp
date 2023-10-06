b4e9ab1467e385258d8a85611460dfa388f21636
KaranoSoraa <kozlov.2013@mail.ru>
2023-10-06 13:51:16 +0300

    Калькулятор.

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -6,25 +6,32 @@ using namespace std;
 
 int main()
 {
-	int x = 3, y = 2;
+	double x, y;
+	cout << "Enter x: ";
+	cin >> x;
 	char oper = ' ';
+	cout << "Enter operation: ";
 	cin >> oper;
+	cout << "Enter y: ";
+	cin >> y;
 
 	cout << "Operation result: ";
-	if (oper == '+') {
+	switch (oper) {
+	case '+':
 		cout << (x + y);
-	}
-	else if (oper == '-') {
+		break;
+	case '-':
 		cout << (x - y);
-	}
-	else if (oper == '*') {
+		break;
+	case '*':
 		cout << (x * y);
-	}
-	else if (oper == '/') {
+		break;
+	case '/':
 		cout << (x / y);
+		break;
+	dafault:
+		cout << "No such operation";
 	}
-	else if (oper == '%') {
-		cout << (x % y);
-	}
+	
 }
 
