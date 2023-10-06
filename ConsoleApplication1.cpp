a83b0c4deac8eb10eec28a21094dffcc8a341816
KaranoSoraa <kozlov.2013@mail.ru>
2023-10-06 13:13:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main()
+{
+	int x = 3, y = 2;
+	char oper = ' ';
+	cin >> oper;
+
+	cout << "Operation result: ";
+	if (oper == '+') {
+		cout << (x + y);
+	}
+	else if (oper == '-') {
+		cout << (x - y);
+	}
+	else if (oper == '*') {
+		cout << (x * y);
+	}
+	else if (oper == '/') {
+		cout << (x / y);
+	}
+	else if (oper == '%') {
+		cout << (x % y);
+	}
+}
+
