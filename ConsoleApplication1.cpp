d090b41a19a9244d490fd521b2d83252abf253c3
KaranoSoraa <kozlov.2013@mail.ru>
2023-10-10 20:32:22 +0300

    Структурный массив.

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,72 +1,38 @@
 ﻿#include <iostream>
-#include <cmath>
 #include <algorithm>
 
 using namespace std;
 
-int a[100];
+struct Numbers {
+	int number;
+	string name;
+};
 
 int main() {
 	int n, m;
 	cin >> n;
+	Numbers nums[100];
 	for (int i = 0; i < n; ++i) {
-		cin >> a[i];
+		string name;
+		int number;
+		cin >> number >> name;
+		nums[i].number = number;
+		nums[i].name = name;
 	}
 	cin >> m;
 	for (int i = 0; i < m; ++i) {
-		char operation;
-		cin >> operation;
-		int l,r;
-		cin >> l >> r;
-		switch (operation) {
-		case 's': {
-			int sum = 0;
-			for (int j = l; j <= r; ++j) {
-				sum += a[j];
-			}
-			cout << sum << endl;
-		}
-		break;
-		}
-		switch (operation) {
-		case 'a': {
-			int sum_a = 0;
-			for (int j = l;j<= r; ++j) {
-				sum_a += a[j];
-			}
-			cout << double(sum_a) / double(r - l + 1) << endl;
-		}
-		break;
-		}
-		switch (operation) {
-		case 'm': {
-			int ma = 0;
-			for (int j = l; j <= r; ++j) {
-				ma = max(ma, a[j]);
-			}
-			cout << ma << endl;
-		}
-		break;
-		case 'r': {
-			reverse(a + l, a + r + 1);
-		}
-		break;
-		case 'u': {
-			sort(a + l, a + r + 1);
-		}
-		break;
-		case 'f': {
-			int x;
-			cin >> x;
-			for (int j = l; j <= r; ++j) {
-				if (a[j] == x) {
-					cout << j << endl;
-					break;
-				}
+		int number;
+		cin >> number;
+		bool fl = 0;
+		for (int j = 0; j < n; ++j) {
+			if (nums[j].number == number) {
+				cout << nums[j].name << endl;
+				fl = 1;
+				break;
 			}
 		}
-		break;
+		if (!fl) {
+			cout << "No such number\n";
 		}
 	}
-}	
-
+}
