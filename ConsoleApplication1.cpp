a187c06c02a87279e4780b3ce78e224e56642a51
KaranoSoraa <kozlov.2013@mail.ru>
2023-10-26 12:08:50 +0300

    exception

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,21 +1,60 @@
 ﻿#include <iostream>
-#include <algorithm>
 
 using namespace std;
 
+enum class Exc {
+	div_error,
+	sqrt_error,
+	log_error,
+
+};
+
+enum class Exc2 {
+	div_error,
+	sqrt_error,
+	log_error,
+
+};
+
+
+int delenie(int x, int y) {
+	if (y != 0) return x / y;
+	throw Exc::div_error;
+}
+
 int main() {
-	int a[100][100];
-	int n, m;
-	cin >> n, m;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> a[i][j];
+	int x, y;
+	cin >> x >> y;
+	try {
+		cout << delenie(x, y);
+	}
+	catch (Exc e) {
+		if (e == Exc::div_error) {
+			cout << "Div 0";
+		}
+		else if (e == Exc::sqrt_error) {
+			cout << "Sqrt exception, wrong art";
 		}
 	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cout << a[i][j] << ' ';
-		}
-		cout << endl;
+	catch (logic_error& e) {
+		cout << "Logic_error" << endl;
+		cout << e.what() << endl;
+		cout << typeid(e).name();
+	}
+	catch (exception& e) {
+		cout << "Exception\n";
+		cout << e.what() << endl;
+		cout << typeid(e).name();
+	}
+	catch (int e) {
+		cout << "Integer\n";
+		cout << e;
+	}
+	catch (const char& e) {
+		cout << "String\n";
+		cout << e;
+	}
+	catch (...) {
+		cout << "Some other exception\n";
 	}
 }
